88b21758abd748d2abb4b9dd36e4c4c51e0c041d
Informatica <informatica@alumno.local>
2026-08-27 08:52:52 -0600

    first commit: producction

---
+++ src/main/java/edu/ingsoft/colegio/gotitas/repository/AuthRepository.java
@@ -0,0 +1,39 @@
+package main.java.edu.ingsoft.colegio.gotitas.repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import main.java.edu.ingsoft.colegio.gotitas.config.DataBaseConnection;
+import java.sql.SQLException;
+import main.java.edu.ingsoft.colegio.gotitas.dto.request.LoginRequest;
+import main.java.edu.ingsoft.colegio.gotitas.dto.response.LoginResponse;
+
+public class AuthRepository {
+
+    private boolean sqlStatus = false;
+
+    public AuthRepository() {
+    }
+    
+
+    // Divide y vencerás: un metodo debe ser engargado de realizar unicamente una tarea específica
+    // el nombre de ese método debe ser modular, directo
+    public LoginResponse findUserByEmail(LoginRequest loginRequest) throws Exception {
+        String sql = "select d.nombre, d.apellido, u.contrasena_hash from usuarios as u right join docentes as d on d.id_docente = u.id_docente where email = ?;";
+
+        try (PreparedStatement pstm = DataBaseConnection.getConnectionDataBase().prepareStatement(sql)) {
+            pstm.setString(1, loginRequest.getEmail());
+            ResultSet rs = pstm.executeQuery();
+
+            if (rs.next()) {
+                return new LoginResponse(rs.getString("nombre"), rs.getString("apellido"), rs.getString("contrasena_hash"));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al encontrar el Email" + e.getMessage());
+        }
+
+        return null;
+        
+    }
+
+}
